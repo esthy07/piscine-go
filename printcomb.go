afa06361382bf23b1be89ca21b9f6025c9bf91fa
esthy07 <nan@iMac-26.local>
2019-06-13 15:38:59 +0000

    printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -5,14 +5,14 @@ import "fmt"
 func PrintComb() {
 	j:=1
 	d:=2
-	for i := 0; i <= 5; i++ {
-		for j = i + 1; j <= 6; j++ {
-			for d = j + 1; d <= 7; d++ {
+	for i := 0; i <= 7; i++ {
+		for j = i + 1; j <= 8; j++ {
+			for d = j + 1; d <= 9; d++ {
 
 				fmt.Print(i)
 				fmt.Print(j)
 				fmt.Print(d)
-				if i < 5 {
+				if i < 7 {
 				fmt.Print(", ")
 				}else {
 				    fmt.Print("\n")
